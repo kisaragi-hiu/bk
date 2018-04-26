7b4c0bf976fdba54bbcee736b535183e8b84fbbf
Kisaragi Hiu <mail@kisaragi-hiu.com>
2018-04-26 16:32:58 +0900

    add a python version

---
+++ bk.py
@@ -0,0 +1,62 @@
+import sys
+import json
+from os import path, getenv
+
+bk_json = path.join(getenv("HOME"), ".bk.json")
+
+def set_entry(key, value):
+    with open(bk_json, "r") as f:
+        tmp = json.load(f)
+    with open(bk_json, "w") as f:
+        tmp[key] = value
+        json.dump(tmp, f)
+
+def retrieve(key):
+    with open(bk_json, "r") as f:
+        print(json.load(f)[key])
+
+def list_entries():
+    S = ""
+    with open(bk_json, "r") as f:
+        for i in json.load(f).keys():
+            S += i + "\n"
+    print(S.strip())
+
+def delete_entry(key):
+    with open(bk_json, "r") as f:
+        tmp = json.load(f)
+    with open(bk_json, "w") as f:
+        del tmp[key]
+        json.dump(tmp, f)
+
+def show_help(exit_code=0):
+    print("""
+bk: key-value pair interface
+bk stores key-value pairs in a JSON file, and provides an interface to retrieve values. The file can be specified with the environment variable $BK_FILE, otherwise it is ~/.bk.json by default.
+Usage:
+  bk <options>
+  bk <key>: retrieve value for <key>
+  bk <key> <value>: set <key> to <value>
+Options:
+  -l, --list
+    list all keys
+  -a, --list-all
+    list all keys with their values
+  -d <key>, --delete <key>
+    delete <key> entry
+  -h, --help
+    show help (this message)""")
+    sys.exit(exit_code)
+
+if len(sys.argv) < 2:
+    show_help(1)
+elif sys.argv[1] in ["--list", "-l"]:
+    list_entries()
+elif sys.argv[1] in ["--delete", "-d"]:
+    delete_entry(sys.argv[2])
+elif sys.argv[1] in ["--help", "-h"]:
+    show_help(0)
+elif len(sys.argv) == 2:
+    retrieve(sys.argv[2])
+else:
+    set_entry(sys.argv[2], sys.argv[3])
